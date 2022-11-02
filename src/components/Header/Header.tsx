02b5b291a397f9b6e3e328c3274cb13fbf912245
Anna Pushkareva <smirnova_spb@mail.ru>
2022-11-02 17:08:26 +0300

    visibility for filter added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    HashRouter as Router,
-    Routes,
-    Route,
-    Link
+    Link,
+    useLocation
 } from "react-router-dom";
 
 import HomeIcon from '../../img/home.png';
@@ -12,7 +10,24 @@ import AddIcon from '../../img/add.png';
 import { Switch } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
+
+const classNames = require('classnames');
+
 const Header: React.FC = () => {
+    const location = useLocation();
+    const [recipeList, setRecipeList] = useState(true);
+
+    useEffect(() => {
+        (location.pathname === "/breakfast" || location.pathname === "/lunch" || location.pathname === "/supper") ? setRecipeList(true) : setRecipeList(false)
+    }, [location.pathname]);
+
+
+    let visible = classNames({
+        'header__favorite-filter invisible': !recipeList,
+        'header__favorite-filter': recipeList,
+    });
+
+
     return (
         <div className='header'>
             <nav className="header__nav">
@@ -29,7 +44,7 @@ const Header: React.FC = () => {
                     </li>
                 </ul>
             </nav>
-            <div className="header__favorite-filter">
+            <div className={visible}>
                 <Switch />
                 <FavoriteIcon className='like header__favorite-like' />
             </div>
