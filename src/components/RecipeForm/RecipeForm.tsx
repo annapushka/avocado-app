00c01f3f20de1e0c4d4382b012e17b689572ea71
Anna Pushkareva <smirnova_spb@mail.ru>
2022-11-07 15:51:32 +0300

    form fixed

--- src/components/RecipeForm/RecipeForm.tsx
+++ src/components/RecipeForm/RecipeForm.tsx
@@ -30,17 +30,18 @@ const RecipeForm: React.FC = () => {
         e.preventDefault();
         newRecipe.date = new Date;
         addRecipe(newRecipe);
+        setNewRecipe({} as RecipeTypes);
     }
 
 
     return (
         <form className='recipeForm' onSubmit={handleSubmit}>
-            <input className='recipeForm__input' name='author' placeholder='Ваше имя' type="text" required onChange={handleChangeData} />
-            <input className='recipeForm__input' name='title' placeholder='Название блюда' type="text" required onChange={handleChangeData} />
-            <input className='recipeForm__input' name='time' placeholder='Время приготовления, мин' type="number" required onChange={handleChangeData} />
-            <input className='recipeForm__input' name='calories' placeholder='Количество калорий, ккал' type="number" required onChange={handleChangeData} />
-            <textarea className='recipeForm__input recipeForm__textarea' name='ingredients' placeholder='Ингридиенты' required onChange={handleChangeData} />
-            <textarea className='recipeForm__input recipeForm__textarea' name='steps' placeholder='Пошаговый рецепт приготовления' required onChange={handleChangeData} />
+            <input className='recipeForm__input' name='author' placeholder='Ваше имя' type="text" required onChange={handleChangeData}  value={newRecipe.author}/>
+            <input className='recipeForm__input' name='title' placeholder='Название блюда' type="text" required onChange={handleChangeData}  value={newRecipe.title}/>
+            <input className='recipeForm__input' name='time' placeholder='Время приготовления, мин' type="number" required onChange={handleChangeData}  value={newRecipe.time}/>
+            <input className='recipeForm__input' name='calories' placeholder='Количество калорий, ккал' type="number" required onChange={handleChangeData}  value={newRecipe.calories}/>
+            <textarea className='recipeForm__input recipeForm__textarea' name='ingredients' placeholder='Ингридиенты' required onChange={handleChangeData}  value={newRecipe.ingredients}/>
+            <textarea className='recipeForm__input recipeForm__textarea' name='steps' placeholder='Пошаговый рецепт приготовления' required onChange={handleChangeData}  value={newRecipe}/>
             <select className='recipeForm__input recipeForm__select' name='meal' onChange={handleChangeData} value={chosenMeal}>
                 <option value="Категория">Категория</option>
                 <option value="Завтрак">Завтрак</option>
